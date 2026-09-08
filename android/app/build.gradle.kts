766c0f5a5904759fc738bf318a9d4d9fc063df85
hmody-web <hmode.qq@gmail.com>
2026-09-09 02:18:50 +0300

    dfsd

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -9,8 +10,10 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
-    compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
-    kotlinOptions { jvmTarget = JavaVersion.VERSION_17.toString() }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 
     defaultConfig {
         applicationId = "com.cinematy.app"
@@ -20,10 +23,24 @@ android {
         versionName = flutter.versionName
     }
 
-    buildTypes { release { signingConfig = signingConfigs.getByName("debug") } }
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
 }
-flutter { source = "../.." }
 
+kotlin {
+    compilerOptions {
+        jvmTarget.set(
+            org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+        )
+    }
+}
+
+flutter {
+    source = "../.."
+}
 
 dependencies {
     implementation("com.github.QWEA0:liquidglass:v2.0.8")
